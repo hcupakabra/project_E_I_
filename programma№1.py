0d9b03e07478893d17e67e0f30616fddb877f5ab
инопришиленец <etalebin@yandex.ru>
2022-01-11 15:28:21 +0800

    new

--- programma№1.py
+++ programma№1.py
@@ -1,6 +1,7 @@
 import pygame
 import pyganim as pyganim
 from pygame import *
+import player
 # у героя должна быть анимка
 # Объявляем переменные
 WIN_WIDTH = 800  # Ширина создаваемого окна
@@ -40,110 +41,11 @@ class Platform(sprite.Sprite):
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 
 
-class Player(sprite.Sprite):
-    def __init__(self, x, y):
-        self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
-        #        Анимация движения вправо
-        boltAnim = []
-        for anim in ANIMATION_LEFT:
-            boltAnim.append((anim, ANIMATION_DELAY))
-        for anim in ANIMATION_RIGHT:
-            boltAnim.append((anim, ANIMATION_DELAY))
-        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
-        self.boltAnimRight.play()
-        #        Анимация движения влево
-        boltAnim = []
-        for anim in ANIMATION_LEFT:
-            boltAnim.append((anim, ANIMATION_DELAY))
-        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
-        self.boltAnimLeft.play()
-
-        self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
-        self.boltAnimStay.play()
-        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
-
-        self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
-        self.boltAnimJumpLeft.play()
-
-        self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
-        self.boltAnimJumpRight.play()
-
-        self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
-        self.boltAnimJump.play()
-
-        self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
-        self.boltAnimJump.play()
-        self.yvel = 0  # скорость вертикального перемещения
-        self.onGround = False  # На земле ли я?
-        sprite.Sprite.__init__(self)
-        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
-        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
-        self.startY = y
-        self.image = Surface((WIDTH, HEIGHT))
-        self.image.fill(Color(COLOR))
-        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
-
-    def update(self, left, right, up, platforms):
-        if up:
-            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
-                self.yvel = -JUMP_POWER
-            self.image.fill(Color(COLOR))
-            self.boltAnimJump.blit(self.image, (0, 0))
-
-        if left:
-            self.xvel = -MOVE_SPEED  # Лево = x- n
-            self.image.fill(Color(COLOR))
-            if up:  # для прыжка влево есть отдельная анимация
-                self.boltAnimJumpLeft.blit(self.image, (0, 0))
-            else:
-                self.boltAnimLeft.blit(self.image, (0, 0))
-
-        if right:
-            self.xvel = MOVE_SPEED  # Право = x + n
-            self.image.fill(Color(COLOR))
-            if up:
-                self.boltAnimJumpRight.blit(self.image, (0, 0))
-            else:
-                self.boltAnimRight.blit(self.image, (0, 0))
-        if not (left or right):  # стоим, когда нет указаний идти
-            self.xvel = 0
-            if not up:
-                self.image.fill(Color(COLOR))
-                self.boltAnimStay.blit(self.image, (0, 0))
-
-        self.onGround = False;  # Мы не знаем, когда мы на земле((
-        self.rect.y += self.yvel
-        self.collide(0, self.yvel, platforms)
-
-        self.rect.x += self.xvel  # переносим свои положение на xvel
-        self.collide(self.xvel, 0, platforms)
-
-    def collide(self, xvel, yvel, platforms):
-        for p in platforms:
-            if sprite.collide_rect(self, p): # если есть пересечение платформы с игроком
-
-                if xvel > 0:                      # если движется вправо
-                    self.rect.right = p.rect.left # то не движется вправо
-
-                if xvel < 0:                      # если движется влево
-                    self.rect.left = p.rect.right # то не движется влево
-
-                if yvel > 0:                      # если падает вниз
-                    self.rect.bottom = p.rect.top # то не падает вниз
-                    self.onGround = True          # и становится на что-то твердое
-                    self.yvel = 0                 # и энергия падения пропадает
-
-                if yvel < 0:                      # если движется вверх
-                    self.rect.top = p.rect.bottom # то не движется вверх
-                    self.yvel = 0                 # и энергия прыжка пропадает
-
-
 def main():
     pygame.init()  # Инициация PyGame, обязательная строчка
     screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
     pygame.display.set_caption("Super Mario Boy")  # Пишем в шапку
-
-    hero = Player(55, 55)  # создаем героя по (x,y) координатам
+    hero = player(55, 55)  # создаем героя по (x,y) координатам
     left = right = False  # по умолчанию — стоим
     up = False
     bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
@@ -179,14 +81,12 @@ def main():
         for e in pygame.event.get():  # Обрабатываем события
             if e.type == KEYDOWN and e.key == K_UP:
                 up = True
-
             if e.type == KEYUP and e.key == K_UP:
                 up = False
             if e.type == KEYDOWN and e.key == K_LEFT:
                 left = True
             if e.type == KEYDOWN and e.key == K_RIGHT:
                 right = True
-
             if e.type == KEYUP and e.key == K_RIGHT:
                 right = False
             if e.type == KEYUP and e.key == K_LEFT:
@@ -208,10 +108,10 @@ def main():
                 x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
             y += PLATFORM_HEIGHT  # то же самое и с высотой
             x = 0  # на каждой новой строчке начинаем с нуля
+        pygame.display.update()  # обновление и вывод всех изменений на экран
         # screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
         hero.update(left, right, up, platforms)   # передвижение
         entities.draw(screen) # отображение всего
-        pygame.display.update()  # обновление и вывод всех изменений на экран
 
 
 if __name__ == "__main__":
