92a180d6ed0cae76c54d64e638347ac049cd86af
gorohovIlya <93247075+gorohovIlya@users.noreply.github.com>
2022-01-15 18:28:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import pygame
 import pygame_menu
-
+import sys
 # инициализация pygame
 pygame.init()
 
@@ -27,8 +28,8 @@ class Character(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # устанавливаем позицию персонажа по x и y
         self.rect.x, self.rect.y = (x, y)
+# запуск анимации, принимает в себя базу изображений
 
-    # запуск анимации, принимает в себя базу изображений
     def startAnimation(self, base_images):
         self.index += 1
         if self.index >= len(base_images):
@@ -44,15 +45,40 @@ class Character(pygame.sprite.Sprite):
             self.rect.x = 315
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 def startGame():
     # база картинок для дракоши
-    images_my_player = [
-        pygame.transform.scale(pygame.image.load('pictures/paint/l1.png'), (100, 100)),
-        pygame.transform.scale(pygame.image.load('pictures/paint/l2.png'), (100, 100)),
-        pygame.transform.scale(pygame.image.load('pictures/paint/l3.png'), (100, 100))
+    pavuk_walk = [
+        load_image("pavuk/walk0.gif", colorkey=-1),
+        load_image("pavuk/walk1.gif", colorkey=-1),
+        load_image("pavuk/walk2.gif", colorkey=-1),
+        load_image("pavuk/walk3.gif", colorkey=-1),
+        load_image("pavuk/walk4.gif", colorkey=-1)
+    ]
+    pavuk_idle = [
+        load_image("pavuk/idle0.gif", colorkey=-1),
+        load_image("pavuk/idle1.gif", colorkey=-1),
+        load_image("pavuk/idle2.gif", colorkey=-1),
+        load_image("pavuk/idle3.gif", colorkey=-1)
     ]
     # создаем нашего героя, что будет ходить (экземпляр класса Character)
-    my_player = Character(315, 150, images_my_player, player_group, all_sprites)
+    my_player = Character(315, 150, pavuk_walk, player_group, all_sprites)
     # часики
     clock = pygame.time.Clock()
     # игра - это цикл
@@ -72,7 +98,7 @@ def startGame():
         # визуализация
         all_sprites.draw(screen)
         # замедляем время
-        clock.tick(10)
+        clock.tick(4)
         # обновление
         pygame.display.flip()
 
@@ -80,226 +106,14 @@ def startGame():
 menu = pygame_menu.Menu("Minotaur's Labyrinth", 500, 500,
                             theme=pygame_menu.themes.THEME_BLUE)
 
-
 menu.add.button('Play', startGame)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(screen)
+
 # import pygame
-# import pyganim as pyganim
 # from pygame import *
-#
-# # Объявляем переменные
-# WIN_WIDTH = 800  # Ширина создаваемого окна
-# WIN_HEIGHT = 640  # Высота
-# MOVE_SPEED = 7
-# WIDTH = 22
-# HEIGHT = 32
+# import pygame_menu
 # COLOR = "#888888"
-# JUMP_POWER = 10
-# GRAVITY = 0.35 # Сила, которая будет тянуть нас вниз
-# DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
-# BACKGROUND_COLOR = "#004400"
-# PLATFORM_WIDTH = 32
-# PLATFORM_HEIGHT = 32
-# PLATFORM_COLOR = "#FF6262"
-# PLATFORM_WIDTH = 32
-# PLATFORM_HEIGHT = 32
-# PLATFORM_COLOR = "#FF6262"
-# ANIMATION_DELAY = 0.1 # скорость смены кадров
-# ANIMATION_RIGHT = [('игрок/r1.png'),
-#                 ('игрок/r2.png'),
-#                 ('игрок/r3.png'),]
-# ANIMATION_LEFT = [('игрок/l1.png'),
-#                 ('игрок/l2.png'),
-#                 ('игрок/l3.png')]
-# ANIMATION_JUMP_LEFT = [('игрок/l4.png', 0.1)]
-# ANIMATION_JUMP_RIGHT = [('игрок/r4.png', 0.1)]
-# ANIMATION_JUMP = [('mario/j.png', 0.1)]
-# ANIMATION_STAY = [('mario/stay.png', 0.1)]
-#
-#
-# class Platform(sprite.Sprite):
-#     def __init__(self, x, y):
-#         sprite.Sprite.__init__(self)
-#         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-#         self.image = image.load("pictures/tile4.png")
-#         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
-#
-#
-# class Player(sprite.Sprite):
-#     def __init__(self, x, y):
-#         self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
-#         #        Анимация движения вправо
-#         boltAnim = []
-#         for anim in ANIMATION_LEFT:
-#             boltAnim.append((anim, ANIMATION_DELAY))
-#         for anim in ANIMATION_RIGHT:
-#             boltAnim.append((anim, ANIMATION_DELAY))
-#         self.boltAnimRight = pyganim.PygAnimation(boltAnim)
-#         self.boltAnimRight.play()
-#         #        Анимация движения влево
-#         boltAnim = []
-#         for anim in ANIMATION_LEFT:
-#             boltAnim.append((anim, ANIMATION_DELAY))
-#         self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
-#         self.boltAnimLeft.play()
-#
-#         self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
-#         self.boltAnimStay.play()
-#         self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
-#
-#         self.boltAnimJumpLeft = pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
-#         self.boltAnimJumpLeft.play()
-#
-#         self.boltAnimJumpRight = pyganim.PygAnimation(ANIMATION_JUMP_RIGHT)
-#         self.boltAnimJumpRight.play()
-#
-#         self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
-#         self.boltAnimJump.play()
-#
-#         self.boltAnimJump = pyganim.PygAnimation(ANIMATION_JUMP)
-#         self.boltAnimJump.play()
-#         self.yvel = 0  # скорость вертикального перемещения
-#         self.onGround = False  # На земле ли я?
-#         sprite.Sprite.__init__(self)
-#         self.xvel = 0  # скорость перемещения. 0 - стоять на месте
-#         self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
-#         self.startY = y
-#         self.image = Surface((WIDTH, HEIGHT))
-#         self.image.fill(Color(COLOR))
-#         self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
-#
-#     def update(self, left, right, up, platforms):
-#         if up:
-#             if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
-#                 self.yvel = -JUMP_POWER
-#             self.image.fill(Color(COLOR))
-#             self.boltAnimJump.blit(self.image, (0, 0))
-#
-#         if left:
-#             self.xvel = -MOVE_SPEED  # Лево = x- n
-#             self.image.fill(Color(COLOR))
-#             if up:  # для прыжка влево есть отдельная анимация
-#                 self.boltAnimJumpLeft.blit(self.image, (0, 0))
-#             else:
-#                 self.boltAnimLeft.blit(self.image, (0, 0))
-#
-#         if right:
-#             self.xvel = MOVE_SPEED  # Право = x + n
-#             self.image.fill(Color(COLOR))
-#             if up:
-#                 self.boltAnimJumpRight.blit(self.image, (0, 0))
-#             else:
-#                 self.boltAnimRight.blit(self.image, (0, 0))
-#         if not (left or right):  # стоим, когда нет указаний идти
-#             self.xvel = 0
-#             if not up:
-#                 self.image.fill(Color(COLOR))
-#                 self.boltAnimStay.blit(self.image, (0, 0))
-#
-#         self.onGround = False;  # Мы не знаем, когда мы на земле((
-#         self.rect.y += self.yvel
-#         self.collide(0, self.yvel, platforms)
-#
-#         self.rect.x += self.xvel  # переносим свои положение на xvel
-#         self.collide(self.xvel, 0, platforms)
-#
-#     def collide(self, xvel, yvel, platforms):
-#         for p in platforms:
-#             if sprite.collide_rect(self, p): # если есть пересечение платформы с игроком
-#
-#                 if xvel > 0:                      # если движется вправо
-#                     self.rect.right = p.rect.left # то не движется вправо
-#
-#                 if xvel < 0:                      # если движется влево
-#                     self.rect.left = p.rect.right # то не движется влево
-#
-#                 if yvel > 0:                      # если падает вниз
-#                     self.rect.bottom = p.rect.top # то не падает вниз
-#                     self.onGround = True          # и становится на что-то твердое
-#                     self.yvel = 0                 # и энергия падения пропадает
-#
-#                 if yvel < 0:                      # если движется вверх
-#                     self.rect.top = p.rect.bottom # то не движется вверх
-#                     self.yvel = 0                 # и энергия прыжка пропадает
-#
-#
-# def main():
-#     pygame.init()  # Инициация PyGame, обязательная строчка
-#     screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
-#     pygame.display.set_caption("Super Mario Boy")  # Пишем в шапку
-#
-#     hero = Player(55, 55)  # создаем героя по (x,y) координатам
-#     left = right = False  # по умолчанию — стоим
-#     up = False
-#     bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
-#     # будем использовать как фон
-#     bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
-#     timer = pygame.time.Clock()
-#     entities = pygame.sprite.Group()  # Все объекты
-#     platforms = []  # то, во что мы будем врезаться или опираться
-#     entities.add(hero)
-#     level = ["-------------------------",
-#              "-                       -",
-#              "-                       -",
-#              "-                       -",
-#              "-            --         -",
-#              "-                       -",
-#              "--                      -",
-#              "-                       -",
-#              "-                   --- -",
-#              "-                       -",
-#              "-                       -",
-#              "-      ---              -",
-#              "-                       -",
-#              "-   -----------         -",
-#              "-                       -",
-#              "-                -      -",
-#              "-                   --  -",
-#              "-                       -",
-#              "-                       -",
-#              "-------------------------"]
-#
-#     while 1:  # Основной цикл программы
-#         timer.tick(60)
-#         for e in pygame.event.get():  # Обрабатываем события
-#             if e.type == KEYDOWN and e.key == K_UP:
-#                 up = True
-#
-#             if e.type == KEYUP and e.key == K_UP:
-#                 up = False
-#             if e.type == KEYDOWN and e.key == K_LEFT:
-#                 left = True
-#             if e.type == KEYDOWN and e.key == K_RIGHT:
-#                 right = True
-#
-#             if e.type == KEYUP and e.key == K_RIGHT:
-#                 right = False
-#             if e.type == KEYUP and e.key == K_LEFT:
-#                 left = False
-#             if e.type == QUIT:
-#                 raise SystemExit("QUIT")
-#         x = y = 0  # координаты
-#         for row in level:  # вся строка
-#             for col in row:  # каждый символ
-#                 if col == "-":
-#                     pf = Platform(x, y)
-#                     entities.add(pf)
-#                     platforms.append(pf)
-#                 elif col == " ":
-#                     pf = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-#                     pf.fill(Color(BACKGROUND_COLOR))
-#                     screen.blit(pf, (x, y))
-#
-#                 x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
-#             y += PLATFORM_HEIGHT  # то же самое и с высотой
-#             x = 0  # на каждой новой строчке начинаем с нуля
-#         # screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
-#         hero.update(left, right, up, platforms)   # передвижение
-#         entities.draw(screen) # отображение всего
-#         pygame.display.update()  # обновление и вывод всех изменений на экран
-#
-#
-# if __name__ == "__main__":
-#     main()
+
+
 
