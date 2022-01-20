936cf6900d8ce34361fef4aaffb80b21d5ec4c15
gorohovIlya <93247075+gorohovIlya@users.noreply.github.com>
2022-01-20 18:54:04 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 import pygame_menu
+import sys
+import os
 from Characters import *
+from level import *
 # инициализация pygame
 pygame.init()
 
@@ -18,13 +21,12 @@ enemy_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join(name)
+    fullname = os.path.join('data', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
-    print(image)
     if colorkey is not None:
         image = image.convert()
         if colorkey == -1:
@@ -35,34 +37,59 @@ def load_image(name, colorkey=None):
     return image
 
 
-animation_LEFT = [
-        load_image("pavuk/walk0.gif", colorkey=-1),
-        load_image("pavuk/walk1.gif", colorkey=-1),
-        load_image("pavuk/walk2.gif", colorkey=-1),
-        load_image("pavuk/walk3.gif", colorkey=-1),
-        load_image("pavuk/walk4.gif", colorkey=-1)]
-animation_IDLE = [
-        load_image("pavuk/idle0.gif", colorkey=-1),
-        load_image("pavuk/idle1.gif", colorkey=-1),
-        load_image("pavuk/idle2.gif", colorkey=-1),
-        load_image("pavuk/idle3.gif", colorkey=-1)]
-animation_RIGHT = [
-    pygame.transform.scale(load_image('Player/r1.png', colorkey=-1), (100, 100)),
-    pygame.transform.scale(load_image('Player/r2.png', colorkey=-1), (100, 100)),
-    pygame.transform.scale(load_image('Player/r3.png', colorkey=-1), (100, 100)),]
-animation_LEFT = [
-    pygame.transform.scale(load_image('Player/l1.png', colorkey=-1), (100, 100)),
-    pygame.transform.scale(load_image('Player/l2.png', colorkey=-1), (100, 100)),
-    pygame.transform.scale(load_image('Player/l3.png', colorkey=-1), (100, 100))]
-animation_STAY = [pygame.transform.scale(load_image('Player/stay.png', colorkey=-1), (100, 100))]
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': pygame.transform.scale(load_image('level_blocks/main_block.png'), (48, 48)),
+    'empty': load_image('level_blocks/background_block.png')
+}
+# player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+tiles_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(100, 15, 100, 100)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
 
 def startGame():
     clock = pygame.time.Clock()
     # игра - это цикл
-
-    my_player = Player(100, 15, 205, 55, [animation_LEFT, animation_STAY, animation_RIGHT], player_group, all_sprites)
-    enemy = Enemy(10, 5, 55, 55, [animation_IDLE, animation_LEFT], enemy_group, all_sprites)
+    player, level_x, level_y = generate_level(load_level('level.txt'))
+    my_player = Player(100, 15, 96, 300, player_group, all_sprites)
+    enemy = Enemy(10, 5, 250, 290, enemy_group, all_sprites)
     running = True
     while running:
         # обработка событий
@@ -73,7 +100,8 @@ def startGame():
                 running = False
         # задаем фон экрана
         screen.fill((0, 0, 0))
-        all_sprites.update()
+        player_group.update()
+        enemy_group.update(my_player)
         # визуализация
         all_sprites.draw(screen)
         # замедляем время
